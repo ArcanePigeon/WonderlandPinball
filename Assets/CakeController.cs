21ed632d9e50571850bced379c49b1718f8fce3d
Kat Marsh <katwmarsh@gmail.com>
2021-02-15 12:43:42 -0500

    fixed cake blender and bunny now grows on collision with cake

--- Assets/CakeController.cs
+++ Assets/CakeController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CakeController : MonoBehaviour
 {
-    Rigidbody renderer;
+    MeshRenderer renderer;
     private GameObject bunny;
     
     private void Start()
     {
-        renderer = gameObject.GetComponent<Rigidbody>();
+        renderer = gameObject.GetComponent<MeshRenderer>();
         bunny = GameObject.Find("Bunny");
     }
 
@@ -17,18 +17,18 @@ public class CakeController : MonoBehaviour
     void Update () 
 	{
          //continuously rotates cuvbe
-		transform.Rotate (new Vector3 (0, 150, 0) * Time.deltaTime);
+		transform.Rotate (new Vector3 (0, 0, 150) * Time.deltaTime);
     }
 
     void OnCollisionEnter(Collision myCollision)
     {
         if (myCollision.gameObject.tag == "Bunny")
         {
-            Destroy (gameObject.FindWithTag("cake"));
+            this.gameObject.GetComponent<MeshRenderer>().enabled = false;
             this.gameObject.GetComponent<BoxCollider>().enabled = false;
 
-            // Vector3 newScale = new Vector3(1.5f, 1.5f, 1.5f);
-            // bunny.transform.localScale = newScale;
+            Vector3 newScale = new Vector3(1.5f, 1.5f, 1.5f);
+            bunny.transform.localScale = newScale;
         }
     }
 }
